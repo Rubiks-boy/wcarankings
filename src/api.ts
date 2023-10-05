87845aa1ebec01b39525902044d147e02c779487
Adam Walker <adamwalker567123@gmail.com>
2023-10-05 01:58:34 -0700

    use correct URL for prod

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "/api"
+      : "http://localhost:8000/api",
   headers: {
     "Content-type": "application/json",
   },
