2afdee9cb43134a4f2e7d1a60d60ff6d72e6ebb2
Adam Walker <adamwalker567123@gmail.com>
2023-10-07 00:26:19 -0700

    useIntersectionObserver hook

---
+++ src/components/List/useIntersectionObserver.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState, RefObject } from "react";
+
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0,
+};
+
+export const useIntersectionObserver = (ref: RefObject<HTMLElement>) => {
+  const [isIntersecting, setIntersecting] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => setIntersecting(entry.isIntersecting),
+      options
+    );
+
+    ref.current && observer.observe(ref.current);
+    return () => observer.disconnect();
+  }, [ref]);
+
+  return isIntersecting;
+};
