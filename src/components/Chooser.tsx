1d33f1536ea987ed2f727eb451deecd24aa22f1c
Adam Walker <adamwalker567123@gmail.com>
2023-10-07 04:48:19 -0700

    select styling

--- src/components/Chooser.tsx
+++ src/components/Chooser.tsx
@@ -23,6 +23,24 @@ type Props = {
   toggleSingle: () => void;
 };
 
+const DownArrowSvg = () => (
+  <svg
+    width="20px"
+    height="20px"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M7 10L12 15L17 10"
+      stroke="#000000"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+);
+
 export const Chooser = ({
   eventId,
   setEventId,
@@ -31,27 +49,34 @@ export const Chooser = ({
 }: Props) => {
   return (
     <div className="chooser">
-      <select
-        name="Event Id"
-        onChange={(e) => setEventId(e.target.value)}
-        value={eventId}
-      >
-        {Object.entries(EVENTS_MAP).map(([eventId, eventName]) => {
-          return (
-            <option key={eventId} value={eventId}>
-              {eventName}
-            </option>
-          );
-        })}
-      </select>
-      <select
-        name="Ranking type"
-        onChange={toggleSingle}
-        value={isSingle ? "single" : "average"}
-      >
-        <option value="single">Single</option>
-        <option value="average">Average</option>
-      </select>
+      <div className="selectInput">
+        <select
+          name="Event Id"
+          onChange={(e) => setEventId(e.target.value)}
+          value={eventId}
+        >
+          {Object.entries(EVENTS_MAP).map(([eventId, eventName]) => {
+            return (
+              <option key={eventId} value={eventId}>
+                {eventName}
+              </option>
+            );
+          })}
+        </select>
+        <DownArrowSvg />
+      </div>
+
+      <div className="selectInput">
+        <select
+          name="Ranking type"
+          onChange={toggleSingle}
+          value={isSingle ? "single" : "average"}
+        >
+          <option value="single">Single</option>
+          <option value="average">Average</option>
+        </select>
+        <DownArrowSvg />
+      </div>
     </div>
   );
 };
