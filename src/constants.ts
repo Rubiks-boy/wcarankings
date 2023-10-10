c1816a99602725f7faefb87b26c21f4f874e067d
Adam Walker <adamwalker567123@gmail.com>
2023-10-10 00:06:00 -0700

    switch to 1000 per scroll page

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ export const PAGE_SIZE = 100;
 export const MAX_PAGES = 3;
 export const ENTRY_HEIGHT = 3.7 * 16 + 2;
 export const NUM_ENTRIES_RENDERED = 25;
-export const ENTRIES_PER_SCROLL_PAGE = 100;
+export const ENTRIES_PER_SCROLL_PAGE = 1000;
 
 export const LOADING_ENTRY = {
   person: {
